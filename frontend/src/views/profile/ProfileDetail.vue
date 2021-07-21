9225024618800127dcb77b23e3e43b366f8f9bf2
woo3gyeob <jongs3030@naver.com>
2021-07-22 02:13:21 +0900

    Design: profileDesign design updated profileDetail 프로필페이지 디자인 변경 Design: followlist design completed followlist 페이지 기본 디자인 구현

--- frontend/src/views/profile/ProfileDetail.vue
+++ frontend/src/views/profile/ProfileDetail.vue
@@ -1,43 +1,47 @@
 <template>
-  <div>
-    <div class="d-flex">
-      <button>
-        뒤로가기버튼
-      </button>
-      <h1>본인프로필명</h1>
+  <div class="m-4">
+    <div class="d-flex m-4" style="justify-content: space-between">
+      <div class="d-flex" style="justify-content: space-between">
+        <button style="padding-right:20px">
+          ←
+        </button>
+        <h1>본인프로필명</h1>
+      </div>
       <button>
         프로필 수정
       </button>
     </div>
     <div class="d-flex">
-      <img src="@/assets/images/profile_default.png" alt="image">
-      <div class="m-4">
+      <img src="@/assets/images/profile_default.png" alt="image" style="width: 70px; height: 70px" >
+      <div class="mx-4">
         <h4>이름</h4>
-        <div class=d-flex>
+        <div class="d-flex" style="justify-content: space-between">
           <button class="d-flex">
             <h4>팔로우</h4>
-            <h4>12</h4>
+            <h4 style="color:blue;">12</h4>
           </button>
+          &nbsp;&nbsp;&nbsp;&nbsp;
           <button class="d-flex">
             <h4>팔로워</h4>
-            <h4>123</h4>
+            <h4 style="color:blue;">123</h4>
           </button>
         </div>
       </div>
     </div>
-    <div>
+    <div class="pt-4">
       <p>본인소개글</p>
-      <p>안녕하세요 저는 싸피 5기 000입니다.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam omnis possimus eveniet alias repudiandae 
+        reprehenderit explicabo pariatur! Incidunt error esse ab animi velit, minus itaque rerum laboriosam tenetur modi sit.</p>
     </div>
     <hr>
 
-    <div class="container">
+    <!-- <div class="container"> -->
       <div class="row">
-        <div v-for="slide in 9" :key="slide" class="col-4">
-          <img src="@/assets/images/img-placeholder.png" alt="image" style="width: 80%; height:90%">
+        <div v-for="slide in 9" :key="slide" class="col-4 my-4">
+          <img src="@/assets/images/img-placeholder.png" alt="image" style="width: 100%; height: 130%">
         </div>
       </div>
-    </div>
+    <!-- </div> -->
   </div>
 </template>
 
