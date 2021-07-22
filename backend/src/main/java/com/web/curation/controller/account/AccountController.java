0df8a6b032b85026c6ac6930ca3f269ac839c374
김백준 <henry9489@gmail.com>
2021-07-22 16:59:39 +0900

    Fix : AccountController

--- backend/src/main/java/com/web/curation/controller/account/AccountController.java
+++ backend/src/main/java/com/web/curation/controller/account/AccountController.java
@@ -80,17 +80,20 @@ public class AccountController {
                                  @RequestParam(required = true) final String newPassword,
                                  @RequestParam(required = true) final String email){
         //이메일, 현재비밀번호, 바꿀 비밀번호 Parameter로 받아옴
-        Optional<User> userOpt = userDao.findUserByEmailAndPassword(email, oldPassword);
-        User user = new User(userOpt.get().getUid(), userOpt.get().getNickname(), email,
-                newPassword, null, null, userOpt.get().getRoles());
-        //디비에 저장 (바뀐 부분만 데이터베이스에 Update된다)
-        userDao.save(user);
-        final BasicResponse result = new BasicResponse();
-        result.status = true;
-        result.data = "success";
-        ResponseEntity response = null;
-        response = new ResponseEntity<>("OK", HttpStatus.OK);
-        return response;
+        Optional<User> userOpt = userDao.findByEmail(email);
+        if(!passwordEncoder.matches(oldPassword, userOpt.get().getPassword())){
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+            User user = new User(userOpt.get().getUid(), userOpt.get().getNickname(), email,
+                    passwordEncoder.encode(newPassword), userOpt.get().getIntroduction(), userOpt.get().getThumbnail(), userOpt.get().getRoles());
+            //디비에 저장 (바뀐 부분만 데이터베이스에 Update된다)
+            userDao.save(user);
+            final BasicResponse result = new BasicResponse();
+            result.status = true;
+            result.data = "success";
+            ResponseEntity response = null;
+            response = new ResponseEntity<>("OK", HttpStatus.OK);
+            return response;
     }
     @GetMapping("/account/checkJWT")
     public String list(){
@@ -111,7 +114,7 @@ public class AccountController {
         Optional<User> userOpt = userDao.findByUid(uid);
         //User객체에 기존의 정보 담아가지고 오고 새로운 닉네임과 소개글로 세팅한다
         User user = new User(uid, nickname, userOpt.get().getEmail(),
-                userOpt.get().getPassword(), introduction, null, userOpt.get().getRoles());
+                userOpt.get().getPassword(), introduction, userOpt.get().getThumbnail(), userOpt.get().getRoles());
         userDao.save(user);
         final BasicResponse result = new BasicResponse();
         result.status = true;
