1613748f960618755588912f947335148115e70e
woo3gyeob <jongs3030@naver.com>
2021-07-22 15:03:35 +0900

    Design: Scrap page HTML done 스크랩 페이지 구현 Fix: profileDetail page HTML fixed 프로필 페이지 그리드 수정

--- frontend/src/views/profile/ProfileDetail.vue
+++ frontend/src/views/profile/ProfileDetail.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="m-4">
-    <div class="d-flex m-4" style="justify-content: space-between">
+    <div class="d-flex my-4" style="justify-content: space-between">
       <div class="d-flex" style="justify-content: space-between">
         <button style="padding-right:20px">
-          ←
+          <b-icon-arrow-left class="h2"></b-icon-arrow-left>
         </button>
-        <h1>본인프로필명</h1>
+        <h2>본인프로필명</h2>
       </div>
       <button>
         프로필 수정
@@ -35,12 +35,15 @@
     </div>
     <hr>
 
+    <div v-for="image in 9" :key="image" class="square img_1">
+    </div>
+
     <!-- <div class="container"> -->
-      <div class="row">
+      <!-- <div class="row">
         <div v-for="slide in 9" :key="slide" class="col-4 my-4">
           <img src="@/assets/images/img-placeholder.png" alt="image" style="width: 100%; height: 130%">
         </div>
-      </div>
+      </div> -->
     <!-- </div> -->
   </div>
 </template>
@@ -52,5 +55,17 @@ export default {
 </script>
 
 <style>
+.square {
+    float:left;
+    position: relative;
+    width: 30%;
+    padding-bottom : 30%; /* = width for a 1:1 aspect ratio */
+    margin:1.66%;
+    background-position:center center;
+    background-repeat:no-repeat;
+    background-size:cover; /* you change this to "contain" if you don't want the images to be cropped */
+}
 
+/* .img_1{background-image:url('img-placeholder.png');} */
+.img_1{background-image:url('../../assets/images/img-placeholder.png');}
 </style>
