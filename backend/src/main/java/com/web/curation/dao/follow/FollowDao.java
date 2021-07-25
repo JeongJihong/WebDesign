1610cffec3ffab0a70bfa5ac028a299880410573
JJH <jjh1731@naver.com>
2021-07-25 20:18:04 +0900

    Feat: 팔로우/팔로잉 정보 확인 및 다른 유저의 프로필 확인

--- backend/src/main/java/com/web/curation/dao/follow/FollowDao.java
+++ backend/src/main/java/com/web/curation/dao/follow/FollowDao.java
@@ -1,8 +1,21 @@
 package com.web.curation.dao.follow;
 
 import com.web.curation.model.follow.Follow;
+import com.web.curation.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface FollowDao extends JpaRepository<Follow, String> {
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowDao extends JpaRepository<Follow, Long> {
     void deleteById(Long followid);
+
+    @Query(value = "SELECT srcid FROM follow f WHERE f.dstid = :id and f.approve = true", nativeQuery = true)
+    List<Long> findByDstidAndApprove(Long id);
+
+    @Query(value = "SELECT dstid FROM follow f WHERE f.srcid = :id and f.approve = true", nativeQuery = true)
+    List<Long> findBySrcidAndApprove(Long id);
+
+    Optional<Follow> findByFollowid(Long followid);
 }
