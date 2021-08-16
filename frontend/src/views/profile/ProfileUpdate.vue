379dd7051c4580b3b17506e3292423cc1943bbcb
woo3gyeob <jongs3030@naver.com>
2021-08-16 18:21:48 +0900

    Fix: profileUpdate nickname dupication test completed

--- frontend/src/views/profile/ProfileUpdate.vue
+++ frontend/src/views/profile/ProfileUpdate.vue
@@ -6,7 +6,7 @@
         <button @click="goBack"><b-icon icon="arrow-left" class="fs-1 me-4"></b-icon></button>
         <span class="fw-bold">프로필 정보 수정</span>
       </span>
-      <button class="text-decoration-none" @click="updateProfileInfo">저장하기</button>
+      <button class="text-decoration-none" v-if="activeButton()" @click="updateProfileInfo">저장하기</button>
     </div>
 
     <!-- 프로필 수정 -->
@@ -22,14 +22,22 @@
         </div>
         
         <!-- Username, 본인 소개글 수정 -->
+        <hr>
         <div>
-          <div class="mt-5 mx-3">
+          <div class="mt-3 mx-3">
             <!-- Username -->
-            <b-form-input v-model="userInfo.nickname" placeholder="username" type="text"></b-form-input>
+            <label for="nickname">닉네임</label>
+            <div class="d-flex flex-row">
+              <b-form-input @change="reNickname()" v-model="userInfo.nickname" id="nickname" placeholder="username" type="text"></b-form-input>
+              &nbsp;&nbsp;
+              <b-button v-if="!checkNicknameValidation" @click="confirmNickname()" style="height:100%" >중복확인</b-button>
+              <b-button v-if="checkNicknameValidation" @click="confirmNickname()" style="height:100%" disabled>중복확인</b-button>
+            </div>
           </div>
           <div class="mt-3 mx-3">
             <!-- 본인 소개글 -->
-            <b-form-textarea v-model="userInfo.introduction" placeholder="본인 소개글" rows="3" max-rows="6"></b-form-textarea>
+            <label for="introduction">소개글</label>
+            <b-form-textarea v-model="userInfo.introduction" id="introduction" placeholder="본인 소개글" rows="3" max-rows="6"></b-form-textarea>
           </div>
         </div>
       </form>
@@ -60,6 +68,9 @@ export default {
       thumbnail: '',
       myUid:'',
       isLoginByKakao: false,
+      nicknameConfirm: true,
+      originalNickname: '',
+      checkNicknameValidation: true,
     }
   },
   methods: {
@@ -89,6 +100,7 @@ export default {
       formData.append("nickname", this.userInfo.nickname);
       formData.append("introduction", this.userInfo.introduction);
       formData.append("thumbnail", this.userInfo.file);
+      console.log(typeof this.userInfo.file, this.userInfo.file)
       axios({
         method: 'patch',
         url: 'http://127.0.0.1:8080/account/profile/',
@@ -109,6 +121,7 @@ export default {
       })
     },
     getUserInfo: function () {
+      console.log('getUserInfo에 있는 닉네임',this.userInfo.nickname)
       axios({
         method: 'get',
         url: `http://127.0.0.1:8080/account/profile/${this.userInfo.nickname}`,
@@ -151,6 +164,60 @@ export default {
         thumbnail: this.thumbnail && require(`@/assets/images/${payload.imgURL}`)
       }
     },
+    reNickname(){
+      this.nicknameConfirm = false
+      this.checkNicknameValidation = false
+      if (this.userInfo.nickname === this.originalNickname) {
+        this.nicknameConfirm = true
+        this.checkNicknameValidation = true
+      }
+    },
+    confirmNickname(){
+      axios({
+        url:'http://127.0.0.1:8080/account/checkNickname',
+        method:'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-AUTH-TOKEN' : this.$store.state.token
+        },
+        params:{
+          nickname: this.userInfo.nickname,
+        }
+      })
+        .then(res=>{
+          console.log(res)
+          if (res.data === 'Fail'){
+            alert('중복된 닉네임 입니다!')
+            // this.userInfo.nickname=""
+            
+          }else{
+            alert('사용 가능한 닉네임 입니다.')
+            this.nicknameConfirm=true
+          }
+        })
+        .catch(err=>{
+          console.log(err)
+        })
+    },
+    activeButton: function(){
+      if(this.userInfo.nickname && this.nicknameConfirm){
+        return true;
+      }else{ return false; }
+    },
+  },
+  watch : {
+    checkNicknameValidation: function () {
+      console.log('실시간', this.userInfo.nickname, this.originalNickname)
+      if (this.userInfo.nickname === this.originalNickname) {
+        return true;
+      }
+      else if (this.nicknameConfirm) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
   },
   created () {
     axios({
@@ -163,6 +230,8 @@ export default {
     })
     .then((res) => {
       this.userInfo.nickname = this.$store.state.username
+      console.log('너 이름이 뭔데 ',this.userInfo.nickname)
+      this.originalNickname = this.$store.state.username
       this.isLoginByKakao = this.$store.state.isLoginByKakao
       this.getUserInfo()
     })
