bec6739592b2bd9f56c7043cf50a60d5189c7876
김백준 <henry9489@gmail.com>
2021-08-17 06:58:44 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-nohup sudo java -jar webcuration-0.0.1-SNAPSHOT.jar &
-=======
-nohup sudo java -jar webcuration-0.0.1-SNAPSHOT.jar &
->>>>>>> f7292986d0b61cd89dbee964d7a18bfd6a293552
+nohup sudo java -jar webcuration-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+
