c5197a86714fe304f05977d4599db2a2900ec376
DESKTOP-KVCQHCD\multicampus <jjh1731@naver.com>
2021-08-19 15:12:00 +0900

    Fix: articletype 구조 변경

--- backend/src/main/java/com/web/curation/model/article/ArticleType.java
+++ backend/src/main/java/com/web/curation/model/article/ArticleType.java
@@ -1,7 +1,20 @@
 package com.web.curation.model.article;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.web.curation.model.comment.Comment;
+import com.web.curation.model.image.Image;
+import com.web.curation.model.user.User;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,6 +23,25 @@ import lombok.*;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 public class ArticleType {
-    Article article;
+    private Long articleid;
+    private Long id;
+    private Long promiseid;
+
+    @CreationTimestamp
+    private LocalDateTime createdtime;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedtime;
+    private String review;
+
+    @OneToMany(mappedBy = "article", cascade={CascadeType.ALL})
+    private List<Image> images = new ArrayList<>();
+
+    @OneToMany(mappedBy = "article", cascade={CascadeType.ALL})
+    private List<Comment> comments = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "id", insertable = false, updatable = false)
+    private User user;
     String type;
 }
