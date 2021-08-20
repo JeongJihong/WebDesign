8f27c88553608184eed2220911168bb97a4f5809
woo3gyeob <jongs3030@naver.com>
2021-08-20 10:17:01 +0900

    Fix: promiseList to original

--- frontend/src/views/promise/PromiseList.vue
+++ frontend/src/views/promise/PromiseList.vue
@@ -10,7 +10,7 @@
 
     <ul class="p-0">
       <p class="mt-3 my-0 mx-3 fs-4 fw-bold">Waiting</p>
-      <div v-if="waitingPromises" class="mt-2 mx-3 px-3 border rounded bg-light">
+      <div v-if="waitingPromises.length !== 0" class="mt-2 mx-3 px-3 border rounded bg-light">
         <div v-for="waitingPromise in waitingPromises"
           :key="waitingPromise.promiseid" class="my-4 mx-1">
           <router-link class="text-decoration-none"
@@ -47,7 +47,7 @@
 
     <ul class="p-0">
       <p class="mt-3 my-0 mx-3 fs-4 fw-bold">Upcoming</p>
-      <div v-if="upcomingPromises" class="mt-2 mx-3 px-3 border rounded bg-light">
+      <div v-if="upcomingPromises.length !== 0" class="mt-2 mx-3 px-3 border rounded bg-light">
         <div v-for="upcomingPromise in upcomingPromises"
           :key="upcomingPromise.promiseid" class="my-4 mx-1">
           <router-link class="text-decoration-none"
